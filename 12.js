3464204c12a1a31d747b9e37ca441f4731ecd785
schn27 <schn27@gmail.com>
2018-12-12 11:21:42 +0300

    refactor day 12

--- 12.js
+++ 12.js
@@ -2,7 +2,7 @@
 
 function calc() {
 	const lines = input.split("\n");
-	const initial = lines[0].match(/[#\.]/g);
+	let state = lines[0].match(/[#\.]/g);
 
 	const rules = lines.slice(2).reduce((a, e) => {
 		const tokens = e.split(" => ");
@@ -10,66 +10,47 @@ function calc() {
 		return a;
 	}, []);
 
-	let state = initial.slice();
-	
-	const getPart1 = 20;
+	const getValue = s => Object.keys(s).map(Number).reduce((a, e) => a + e, 0);
+
+	const genPart1 = 20;
 	let part1 = null;
 
 	const gen1 = 1000;
 	const gen2 = 5000;
-	let valueGen1 = null;
+	let gen1Value = null;
 
-	for (let i = 0; i < gen2; ++i) {
-		if (i == getPart1) {
-			part1 = Object.keys(state).map(Number).reduce((a, e) => a + e, 0);
+	for (let gen = 0; gen < gen2; ++gen) {
+		if (gen == genPart1) {
+			part1 = getValue(state);
 		}
 
-		if (i == gen1) {
-			valueGen1 = Object.keys(state).map(Number).reduce((a, e) => a + e, 0);
+		if (gen == gen1) {
+			gen1Value = getValue(state);
 		}
 
 		state = nextGen(state, rules);
 	}
 
-	let valueGen2 = Object.keys(state).map(Number).reduce((a, e) => a + e, 0);
-	let part2 = valueGen1 + (valueGen2 - valueGen1) / (gen2 - gen1) * (50000000000 - gen1);
+	const part2 = gen1Value + (getValue(state) - gen1Value) / (gen2 - gen1) * (50000000000 - gen1);
 
 	return part1 + " " + part2;
 }
 
 function nextGen(state, rules) {
-	let next = [];
-
-	let keys = Object.keys(state).map(Number);
-	let min = Math.min(...keys);
-	let max = Math.max(...keys);
+	const keys = Object.keys(state).map(Number);
+	const min = Math.min(...keys);
+	const max = Math.max(...keys);
+	const next = [];
 
 	for (let k = min - 2; k <= max + 2; ++k) {
-		const pent = [state[k - 2] || ".", state[k - 1] || ".", state[k] || ".", state[k + 1] || ".", state[k + 2] || "."].join("");
-		if (rules[pent] == "#") {
+		const group = [state[k - 2] || ".", state[k - 1] || ".", state[k] || ".", state[k + 1] || ".", state[k + 2] || "."].join("");
+		if (rules[group] == "#") {
 			next[k] = "#";
 		}
 	}
 
 	return next;
 }
-
-const test = `initial state: #..#.#..##......###...###
-
-...## => #
-..#.. => #
-.#... => #
-.#.#. => #
-.#.## => #
-.##.. => #
-.#### => #
-#.#.# => #
-#.### => #
-##.#. => #
-##.## => #
-###.. => #
-###.# => #
-####. => #`;
 
 const input = `initial state: ###..#...####.#..###.....####.######.....##.#####.##.##..###....#....##...##...##.#..###..#.#...#..#
 
